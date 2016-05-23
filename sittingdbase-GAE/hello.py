03cbef8f1b49c3e036df7c133bb6bbccb1bff7de
Jaden Yuros <jaden.yuros@outlook.com>
2016-05-22 22:25:59 -0700

    Revert "Initial File Add"
    This reverts commit e2ee69303404a79389295c27b1fcb9d0a78d2b4b.

--- sittingdbase-GAE/hello.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-from google.appengine.ext import ndb
-import db_defs
-import json
-
-class Hello(webapp2.RequestHandler):
-	def get(self):
-		self.response.write("Hello Sitting World - try /stretch or /encourage")
--- sittingdbase-GAE/main.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-application = webapp2.WSGIApplication([
-	('/', 'hello.Hello'),
-	('/encourage', 'encourage.Encouragement'),
-	('/stretch', 'stretch.Stretch'),
-	('/addstretch', 'admin.AddStretch'),
-	('/addencouragement', 'admin.AddEncouragement'),
-], debug=True)
